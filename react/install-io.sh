fdd323b1adff1ad344082853d0c5ec1fb0e54966
Ignacio Carbajo <icarbajop@gmail.com>
2015-09-03 20:49:48 +0200

    Fix installation script

--- react/install-io.sh
+++ react/install-io.sh
@@ -1,4 +1,5 @@
 # This should be temporal, till the provision file works
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
-. ~/.bashrc; nvm install iojs; nvm use iojs-v3.3.0
+. ~/.bashrc; nvm install iojs
+# nvm use iojs-v3.3.0 # Fix to use current version
